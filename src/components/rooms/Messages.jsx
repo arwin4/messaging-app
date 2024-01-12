723f0773846351e081f830c806231f43aa9f881f
Arwin Yella <16850195+arwin4@users.noreply.github.com>
2024-01-12 11:31:40 +0100

    Show messages live using socket.io

--- src/components/rooms/Messages.jsx
+++ src/components/rooms/Messages.jsx
@@ -1,18 +1,18 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 
-export default function Messages({ room }) {
+export default function Messages({ room, socketMessages }) {
   const { messages } = room;
 
-  return (
-    <>
-      {messages.map((message) => (
-        <div key={message._id} className="message">
-          <div className="date-created">{message.dateCreated}</div>
-          <div className="author">{message.author.username}</div>
-          <div className="text-content">{message.content.textContent}</div>
-        </div>
-      ))}
-    </>
-  );
+  const fetchAndSocketMessages = messages.concat(socketMessages);
+
+  const messagesElement = fetchAndSocketMessages.map((message) => (
+    <div key={message._id} className="message">
+      <div className="date-created">{message.dateCreated}</div>
+      <div className="author">{message.author.username}</div>
+      <div className="text-content">{message.content.textContent}</div>
+    </div>
+  ));
+
+  return messagesElement;
 }
--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -1,13 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import useRoom from '../hooks/auth/rooms/useRoom';
 import Title from '../components/rooms/Title';
 import Messages from '../components/rooms/Messages';
 import MessagesForm from '../components/rooms/MessageForm';
+import { socket } from '../socket.io/socket';
 
 export default function Room() {
   const { id } = useParams();
   const { room, loading: loadingRoom, error: fetchError } = useRoom(id);
+
+  // When the socket receives a new message, it is pushed in this array
+  const [socketMessages, setSocketMessages] = useState([]);
+
+  // Socket
+  useEffect(() => {
+    socket.connect();
+
+    // The handlers must be named, or they cannot be removed. Strict mode will
+    // run the effect twice, so the handlers must be removed before remounting!
+    // Or else they'll handle all events twice.
+    function setupListener() {
+      socket.emit('join-room', id);
+      socket.emit('listen-for-messages');
+    }
+
+    function handleNewMessage(message) {
+      setSocketMessages((previous) => [...previous, message]);
+    }
+
+    socket.on('connect', setupListener);
+    socket.on('new-message', handleNewMessage);
+
+    return () => {
+      // Close connection when unmounting
+      socket.disconnect();
+      // Remove event listeners
+      socket.off('new-message', handleNewMessage);
+      socket.off('connect', setupListener);
+    };
+  }, []);
 
   if (fetchError)
     return <>There was an error loading the conversation: {fetchError}</>;
@@ -16,7 +48,7 @@ export default function Room() {
   return (
     <main>
       <Title room={room} />
-      <Messages room={room} />
+      <Messages room={room} socketMessages={socketMessages} />
       <MessagesForm room={room} />
     </main>
   );
