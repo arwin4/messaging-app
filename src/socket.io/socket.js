e85e789e4167c2ede6763f424fffcabfa0b7a08e
Arwin Yella <16850195+arwin4@users.noreply.github.com>
2024-01-12 15:13:21 +0100

    Fix 400 Bad Request

--- src/socket.io/socket.js
+++ src/socket.io/socket.js
@@ -1,7 +1,6 @@
 import { io } from 'socket.io-client';
 
-// "undefined" means the URL will be computed from the `window.location` object
-const URL =
-  process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:4000';
-
-export const socket = io(URL, { autoConnect: false });
+export const socket = io(import.meta.env.VITE_API_SERVER_URL, {
+  autoConnect: false,
+  transports: ['websocket'],
+});
