ce6a78e9f859ce29122213f4358818ef1eb82ac3
Arwin Yella <16850195+arwin4@users.noreply.github.com>
2024-01-14 17:25:01 +0100

    Extract fetchAndSetCurrentUser

--- src/components/auth/RequireAuth.jsx
+++ src/components/auth/RequireAuth.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import useAuth from '../../hooks/auth/useAuth';
-import getJwt from '../../utils/getJwt';
+import fetchAndSetCurrentUser from '../../utils/fetch/fetchAndSetCurrentUser';
 
 export default function RequireAuth({ children }) {
   const { authed, setAuthed } = useAuth();
@@ -17,19 +17,7 @@ export default function RequireAuth({ children }) {
 
     if (jwt) {
       setAuthed(true);
-
-      const fetchCurrentUser = async () => {
-        const res = await fetch(
-          `${import.meta.env.VITE_API_SERVER_URL}/users/`,
-          {
-            method: 'GET',
-            headers: { Authorization: `Bearer ${getJwt()}` },
-          },
-        );
-        const user = await res.json();
-        localStorage.setItem('user', JSON.stringify(user));
-      };
-      fetchCurrentUser();
+      fetchAndSetCurrentUser();
     }
   }, []);
 
