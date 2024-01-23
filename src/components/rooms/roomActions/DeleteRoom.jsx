45b1e332d86214a057463937266988ce05a5082f
Arwin Yella <16850195+arwin4@users.noreply.github.com>
2024-01-23 14:08:31 +0100

    Split RoomActions into components

---
+++ src/components/rooms/roomActions/DeleteRoom.jsx
@@ -0,0 +1,56 @@
+/* eslint-disable react/prop-types */
+import React, { useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
+import deleteRoom from '../../../utils/fetch/deleteRoom';
+
+export default function DeleteRoom({ room }) {
+  const navigate = useNavigate();
+  const deleteRoomModal = useRef();
+
+  function openDeleteConversationModal() {
+    deleteRoomModal.current.showModal();
+  }
+
+  function closeDeleteConversationModal() {
+    deleteRoomModal.current.close();
+  }
+
+  async function handleDeleteConversation() {
+    const res = await deleteRoom(room._id);
+    if (res.status !== 200) {
+      // TODO: Handle error
+    }
+    navigate('/dashboard');
+  }
+
+  return (
+    <>
+      <button
+        type="button"
+        className="delete-room"
+        onClick={openDeleteConversationModal}
+      >
+        Delete conversation
+      </button>
+
+      <dialog ref={deleteRoomModal}>
+        <form onSubmit={handleDeleteConversation}>
+          <p>
+            If you delete this conversation, it will be forever lost to all
+            participants.
+          </p>
+          <p>Are you sure you want to delete this conversation?</p>
+
+          <menu>
+            <button formMethod="dialog" type="submit">
+              Yes, delete
+            </button>
+            <button type="button" onClick={closeDeleteConversationModal}>
+              Cancel
+            </button>
+          </menu>
+        </form>
+      </dialog>
+    </>
+  );
+}
