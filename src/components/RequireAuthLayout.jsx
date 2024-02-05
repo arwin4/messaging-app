43aafe0574fb1f2612a304543225ec0eceb6c679
Arwin Yella <16850195+arwin4@users.noreply.github.com>
2024-02-05 15:38:52 +0100

    Move global styles out of RequireAuthLayout

--- src/components/RequireAuthLayout.jsx
+++ src/components/RequireAuthLayout.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './style/RequireAuthLayout.css';
 import { Outlet } from 'react-router-dom';
 import Navigation from './Navigation';
 import RequireAuth from './auth/RequireAuth';
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { AuthProvider } from '@hooks/auth/useAuth';
 import Router from './Router';
+import './style/main.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
