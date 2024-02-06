486c35fe1bc7a7fcf1f089b1d7b6645a49ea5246
Arwin Yella <16850195+arwin4@users.noreply.github.com>
2024-02-06 15:22:36 +0100

    Style messages
     - Format message timestamps

--- src/components/rooms/Messages.jsx
+++ src/components/rooms/Messages.jsx
@@ -1,17 +1,42 @@
 import React from 'react';
+import { format, formatRelative } from 'date-fns';
+import './style/Messages.css';
+import getCurrentUser from '@utils/getCurrentUser';
 
 export default function Messages({ room, socketMessages }) {
   const { messages } = room;
+  const currentUser = getCurrentUser();
 
   const fetchAndSocketMessages = messages.concat(socketMessages);
 
-  const messagesElement = fetchAndSocketMessages.map((message) => (
-    <div key={message._id} className="message">
-      <div className="date-created">{message.dateCreated}</div>
-      <div className="author">{message.author.username}</div>
-      <div className="text-content">{message.content.textContent}</div>
-    </div>
-  ));
+  const messagesElement = fetchAndSocketMessages.map((message) => {
+    const isCurrentUserAuthorOfMessage =
+      message.author.username === currentUser.username;
+
+    // Format timestamps like: 'Last Thursday at 23:00'.
+    // If the message is from today, leave out 'Today at' text.
+    const humanReadableTimestamp =
+      new Date(message.dateCreated).toDateString() === new Date().toDateString()
+        ? format(message.dateCreated, 'H:mm')
+        : formatRelative(message.dateCreated, Date.now());
+
+    return (
+      <div
+        key={message._id}
+        className={`message ${
+          isCurrentUserAuthorOfMessage ? 'current-user' : ''
+        }`}
+      >
+        <div className="metadata">
+          <div className="author">
+            {isCurrentUserAuthorOfMessage ? 'You' : message.author.username}
+          </div>
+          <div className="date-created">{humanReadableTimestamp}</div>
+        </div>
+        <div className="text-content">{message.content.textContent}</div>
+      </div>
+    );
+  });
 
   return <div className="messages">{messagesElement}</div>;
 }
