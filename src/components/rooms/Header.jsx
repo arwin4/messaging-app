d4683ba4ef58d638fd50167b9bb3e104e43738e4
Arwin Yella <16850195+arwin4@users.noreply.github.com>
2024-02-15 14:09:17 +0100

    Remove BackToRoomsButton

--- src/components/rooms/Header.jsx
+++ src/components/rooms/Header.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import roomPropType from '@components/propTypes/roomPropType';
 import socketMessagesPropType from '@components/propTypes/socketMessagesPropType';
-import BackToRoomsButton from './buttons/BackToRoomsButton';
 import RoomSettings from './RoomSettings';
 import '../style/Header.css';
 
 export default function Header({ room, socketMessages }) {
   return (
     <header className="room-header">
-      <BackToRoomsButton />
       <h1>Chat</h1>
       <RoomSettings room={room} socketMessages={socketMessages} />
     </header>
