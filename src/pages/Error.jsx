87f724565802dbe2c47ecfb4f5d041596be325ae
Arwin Yella <16850195+arwin4@users.noreply.github.com>
2024-02-21 14:07:46 +0100

    Add link buttons

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,31 +1,59 @@
 import React from 'react';
-import { Link, Navigate, useRouteError } from 'react-router-dom';
+import { Link, useNavigation, useRouteError } from 'react-router-dom';
 import './style/Error.css';
+import LinkButton from '@components/buttons/LinkButton';
 
 export default function Error() {
   const error = useRouteError();
+  const navigation = useNavigation();
+
+  const backToChatsBtn = (
+    <Link to="/">
+      <LinkButton
+        icon="ri:chat-4-line"
+        text="Back to chats"
+        inline="true"
+        // Set busy state when navigating away
+        busy={Boolean(navigation.location?.pathname)}
+      />
+    </Link>
+  );
+
+  const toLoginBtn = (
+    <Link to="/login">
+      <LinkButton
+        icon="ri:login-box-line"
+        text="Log in again"
+        inline="true"
+        // Set busy state when navigating away
+        busy={Boolean(navigation.location?.pathname)}
+      />
+    </Link>
+  );
 
   switch (error.status) {
     case 400:
       return (
         <div className="error">
-          {error.data} <Link to="/">Back to homepage</Link>{' '}
+          {error.data} {backToChatsBtn}
         </div>
       );
 
     case 401:
       // Unauthorized
-      return <Navigate to="/login" />;
+      return { toLoginBtn };
     case 404:
       return (
         <div className="error">
-          404 Not Found. <Link to="/">Back to homepage</Link>
+          404 Not Found.
+          {backToChatsBtn}
         </div>
       );
     default:
       return (
         <div className="error">
-          An error occurred. <Link to="/login">Go to login</Link>
+          An error occurred.
+          {backToChatsBtn}
         </div>
       );
   }
